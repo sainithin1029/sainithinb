c9ee970002e11742fe63322b78258d7a923e0887
sainithin1029 <sainithin1029@gmail.com>
2023-06-07 16:10:27 -0500

    new

--- Assignments/Assignments_new/src/main/java/June06/BigAndSmall.java
+++ Assignments/Assignments_new/src/main/java/June06/BigAndSmall.java
@@ -5,19 +5,17 @@ public class BigAndSmall {
 
 
         int[] arr = {2, 4, 1, 6, 8,-1,253,3};
-        int max = 0, min = arr[0];
-        for (int i = 0; i < arr.length; i++) {
+        int max = arr[0], min = arr[0];
+        for (int i = 1; i < arr.length; i++) {
             if (arr[i] > max) {
                 max = arr[i];
             }
-
-        }
-        for (int i = 1; i < arr.length; i++) {
             if (arr[i] < min) {
                 min = arr[i];
             }
 
         }
+
         System.out.println("Max:" + max);
         System.out.println("Min:" + min);
     }

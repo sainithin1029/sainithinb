4034d310f8a1a575d2095fad6410fc098db29d15
sainithin1029 <sainithin1029@gmail.com>
2023-06-19 15:11:52 -0500

    test1

--- Assignments/Assignments_new/src/main/java/June15/Avg.java
+++ Assignments/Assignments_new/src/main/java/June15/Avg.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Avg {
     public static void main(String args[]){
-        int[] a={3,9,29,38,24,48,94};
+        int[] a={3,9,29,38,24,48,98};
         System.out.println(Arrays.stream(a).filter(x->x%2!=0).map(x->x*x).average().toString());
     }
 }
